d90583e7db71a312782a4018890d77ca362eb3b5
vzhufeng <vzhufeng@163.com>
2021-05-23 20:19:09 +0800

    feat(routes): update log

--- routes/log.js
+++ routes/log.js
@@ -1,34 +1,42 @@
 const Router = require('koa-router');
-
+const querystring = require('querystring');
 const router = new Router();
 
 router.register(['/log'], ['GET', 'POST'], async (ctx) => {
-  // referer
-  // referer: DataTypes.STRING,
-
   const { method, query, body, headers, socket } = ctx.request;
   const time = new Date().getTime();
 
   const param = {
     ip: headers['x-forwarded-for'] || socket.remoteAddress,
-    referer,
+    referer: headers['referer'] || '',
     time,
   };
 
+  let appId, content;
+
   if ('GET' === method.toUpperCase()) {
-    const { appId, content } = query;
-    param.id = appId;
-    param.content = content;
+    appId = query.appId;
+    content = query.content;
   } else if ('POST' === method.toUpperCase()) {
-    const { appId, content } = body;
-    param.id = appId;
-    param.content = content;
+    if (headers['content-type'].includes('text/plain')) {
+      const _body = querystring.parse(body);
+      appId = _body.appId;
+      content = _body.content;
+    } else if (headers['content-type'].includes('application/json')) {
+      appId = body.appId;
+      content = body.content;
+    }
   }
+
+  param.content = content;
+
   if (ctx.cache[`${appId}`]) {
     ctx.cache[`${appId}`].push(param);
   } else {
     ctx.cache[`${appId}`] = [param];
   }
+
+  ctx.body = 'ok';
 });
 
 module.exports = router.routes();
